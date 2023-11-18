9423f65c40ee8155b60a577cb97aa1daba3b9ba3
soohyun <developer.shkim@gmail.com>
2023-11-18 16:09:00 +0900

    11주차: 게시글 페이지네이션, 상세

--- routes/posts.js
+++ routes/posts.js
@@ -16,4 +16,10 @@ router.get('/insert', function(req, res){
   res.render('index', {title:'글쓰기', pageName:'posts/insert.ejs'});
 })
 
+//글쓰기
+router.get('/read', function(req, res){
+  const id = req.query.id;
+  res.render('index', {title:'게시글 정보', pageName:'posts/read.ejs', id });
+})
+
 module.exports = router;
